5980d030c91a096b09e76e491bdec4d492aaad3d
varunj <deepikay@iiitd.ac.in>
2017-06-19 11:21:42 +0530

    Added Oberservations in README

--- 4lstm_train.py
+++ 4lstm_train.py
@@ -11,13 +11,15 @@ from pprint import pprint
 import pdb
 np.set_printoptions(threshold=np.nan)
 from keras.models import Sequential
+from keras import optimizers	
 from keras.layers import Dense
 from keras.layers import LSTM
 from keras.utils import plot_model
 import pickle
 
+CLASSES = ('bloom','click')
 # CLASSES = ('zooin','zoout')
-CLASSES = ('bloom','click','zooin','zoout')
+# CLASSES = ('bloom','click','zooin','zoout')
 NOS_CLASSES = len(CLASSES)
 
 def shuffle_data(labels, seq):
@@ -45,19 +47,6 @@ for x,y in dic.items():
 		if (CLASSES[i]==x[:5]):
 			targetarr[i] = 1
 	targetSeq.append(targetarr)
-
-dic = pickle.load(open("./result_dics/dic_zoom_2d_100.pickle", "rb" ))
-for x,y in dic.items():
-	y = y.reshape(100, 42)
-	y = np.transpose(y)
-	y = y.reshape(1, 42, 100)
-	dataSeq.append(y)
-	targetarr = np.zeros(NOS_CLASSES)
-	for i in range(0, NOS_CLASSES):
-		if (CLASSES[i]==x[:5]):
-			targetarr[i] = 1
-	targetSeq.append(targetarr)
-
 dic = pickle.load(open("./result_dics/dic_bloomclick_2_2d_100.pickle", "rb" ))
 for x,y in dic.items():
 	y = y.reshape(100, 42)
@@ -70,17 +59,28 @@ for x,y in dic.items():
 			targetarr[i] = 1
 	targetSeq.append(targetarr)
 
-dic = pickle.load(open("./result_dics/dic_zoom_2_2d_100.pickle", "rb" ))
-for x,y in dic.items():
-	y = y.reshape(100, 42)
-	y = np.transpose(y)
-	y = y.reshape(1, 42, 100)
-	dataSeq.append(y)
-	targetarr = np.zeros(NOS_CLASSES)
-	for i in range(0, NOS_CLASSES):
-		if (CLASSES[i]==x[:5]):
-			targetarr[i] = 1
-	targetSeq.append(targetarr)
+# dic = pickle.load(open("./result_dics/dic_zoom_2d_100.pickle", "rb" ))
+# for x,y in dic.items():
+# 	y = y.reshape(100, 42)
+# 	y = np.transpose(y)
+# 	y = y.reshape(1, 42, 100)
+# 	dataSeq.append(y)
+# 	targetarr = np.zeros(NOS_CLASSES)
+# 	for i in range(0, NOS_CLASSES):
+# 		if (CLASSES[i]==x[:5]):
+# 			targetarr[i] = 1
+# 	targetSeq.append(targetarr)
+# dic = pickle.load(open("./result_dics/dic_zoom_2_2d_100.pickle", "rb" ))
+# for x,y in dic.items():
+# 	y = y.reshape(100, 42)
+# 	y = np.transpose(y)
+# 	y = y.reshape(1, 42, 100)
+# 	dataSeq.append(y)
+# 	targetarr = np.zeros(NOS_CLASSES)
+# 	for i in range(0, NOS_CLASSES):
+# 		if (CLASSES[i]==x[:5]):
+# 			targetarr[i] = 1
+# 	targetSeq.append(targetarr)
 
 print(len(dataSeq))
 seq = np.arange(len(dataSeq))
@@ -94,12 +94,14 @@ print(data.shape)
 print(target.shape)
 
 model = Sequential()  
-model.add(LSTM(200, input_shape=(42,100), return_sequences=True))
-model.add(Flatten())
+model.add(LSTM(200, input_shape=(42,100), return_sequences=False))
+# model.add(Flatten())
 model.add(Dense(NOS_CLASSES, activation='softmax'))
 
+# sgd = optimizers.SGD(lr=0.01, clipvalue=0.5)
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-history = model.fit(data, target, epochs=300, batch_size=10, verbose=2, validation_split=0.30)
+# print(model.summary())
+history = model.fit(data, target, epochs=300, batch_size=5, verbose=2, validation_split=0.30)
 
 # model.save('my_model.h5')
 # model.save_weights('my_model_weights.h5')
@@ -117,3 +119,16 @@ plt.ylabel('loss')
 plt.xlabel('epoch')
 plt.legend(['train', 'test'], loc='upper left')
 plt.savefig('loss.png')
+
+
+
+
+
+
+
+'''
+zooin + zoout
+acc 100/96 in ~300 epochs
+history = model.fit(data, target, epochs=3000, batch_size=5, verbose=2, validation_split=0.30)
+
+'''
--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # TCS_HandPose_2D
 Estimating 3D Hand Pose from RGB Images
 Find results at https://github.com/varunj/TCS_HandPose_2D_Imgs
+
+
+BAD: may decrease acc
+bloom 25, 36, 37, 43, 51, 52-75
+click 44, 46, 48-50, 54-56, 58-62, 74-75, 78-100
+zooin 5, 15, 72, 
+zoout 1-6, 47, 49, 57, 
+
+GOOD: Can use for images
+bloom 2_25, 2_75, 2_115, 30_2, 30_70, 30_168
+click 39_1, 39_109, 65_9, 65_81, 68_1, 68_69, 77_8, 77_122 
+zooin 8, 35_161, 36_45, 36_136, 38_135, 45_76, 49_82, 50_10, 50_112, 62_21, 63_82
+zoout 30_20, 30_143, 38_24, 38_132, 80_1, 82_77
